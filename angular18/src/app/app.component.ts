950a86a792e7ecde98fceaa4cba0398bf19c6a7e
Arun Sakthi Anand M <arunsakthianand@gmail.com>
2024-07-24 21:59:28 +0530

    Updated app component

--- angular18/src/app/app.component.ts
+++ angular18/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponent } from './first/first.component';
+import { ControlFlowComponent } from './control-flow/control-flow.component';
+import { SignalsComponent } from './signals/signals.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponent],
+  imports: [
+    RouterOutlet,
+    FirstComponent,
+    ControlFlowComponent,
+    SignalsComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular18';
